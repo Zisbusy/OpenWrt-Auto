f58a74119d345103d99a65bbd260265aabb3fcfc
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2024-07-28 17:28:43 +0800

    Update run.sh

--- 23.05.3/run.sh
+++ 23.05.3/run.sh
@@ -51,6 +51,10 @@ echo "删除访问主页跳转提示文案..." | tee -a /home/openwrt-auto/log.t
 rm -rf /home/openwrt/feeds/luci/modules/luci-base/root/www/index.html
 cp -rf /home/openwrt-auto/23.05.3/package/luci/index.html /home/openwrt/feeds/luci/modules/luci-base/root/www/
 
+echo "修改 UPnP 默认 ip..." | tee -a /home/openwrt-auto/log.txt
+rm -rf /home/openwrt/package/feeds/luci/luci-app-upnp/htdocs/luci-static/resources/view/upnp/upnp.js
+cp -rf /home/openwrt-auto/23.05.3/package/upnp/upnp.js /home/openwrt/package/feeds/luci/luci-app-upnp/htdocs/luci-static/resources/view/upnp/
+
 echo "设置：时区 Asia/Shanghai、NTP 服务器、默认网关、主机名......" | tee -a /home/openwrt-auto/log.txt
 rm -rf /home/openwrt/package/base-files/files/bin/config_generate
 if [ "$1" == "x86" ] ;then
